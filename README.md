bbc5515da681a5f7beebe7444edc0621a34550fc
mocnii <msestak@irb.hr>
2016-07-14 12:32:51 +0200

    Add skeleton files by Minilla

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+FindOrigin - It's new $module
+
+# SYNOPSIS
+
+    use FindOrigin;
+
+# DESCRIPTION
+
+FindOrigin is ...
+
+# LICENSE
+
+Copyright (C) mocnii.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mocnii <msestak@irb.hr>
---
+++ lib/FindOrigin.pm
@@ -0,0 +1,39 @@
+package FindOrigin;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+FindOrigin - It's new $module
+
+=head1 SYNOPSIS
+
+    use FindOrigin;
+
+=head1 DESCRIPTION
+
+FindOrigin is ...
+
+=head1 LICENSE
+
+Copyright (C) mocnii.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mocnii E<lt>msestak@irb.hrE<gt>
+
+=cut
+
